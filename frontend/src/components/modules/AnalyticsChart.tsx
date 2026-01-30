f3d5531fc7fc568b8fed5add5f35abf361a35713
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:30:58 +0100

    feat: define generic props for AnalyticsChart

--- frontend/src/components/modules/AnalyticsChart.tsx
+++ frontend/src/components/modules/AnalyticsChart.tsx
@@ -1,3 +1,8 @@
 // AnalyticsChart Component Framework
 import React, { useEffect, useState } from 'react';
 import styles from './AnalyticsChart.module.css';
+
+interface AnalyticsChartProps {
+  id: string;
+  debug?: boolean;
+}
