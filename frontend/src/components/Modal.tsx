27b6198442e03636c009619f469a2f1e97b219cf
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 18:46:33 +0100

    feat: define types for Modal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styles from './Modal.module.css';
 
 // Modal Component Definition
+
+interface ModalProps {
+  title?: string;
+  isActive?: boolean;
+  onAction?: () => void;
+}
