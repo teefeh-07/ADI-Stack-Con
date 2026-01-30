d30aa9efc225d208b4a4867e75877d1ab02e4de8
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:18:07 +0100

    feat: define generic props for TokenSwap

--- frontend/src/components/modules/TokenSwap.tsx
+++ frontend/src/components/modules/TokenSwap.tsx
@@ -1,3 +1,8 @@
 // TokenSwap Component Framework
 import React, { useEffect, useState } from 'react';
 import styles from './TokenSwap.module.css';
+
+interface TokenSwapProps {
+  id: string;
+  debug?: boolean;
+}
