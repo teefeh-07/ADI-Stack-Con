c656a79ed64f4806602c32b9c062874886b2a263
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:30:16 +0100

    feat: add side-effect logic to AdminPanel

--- frontend/src/components/modules/AdminPanel.tsx
+++ frontend/src/components/modules/AdminPanel.tsx
@@ -15,3 +15,7 @@ interface State {
 
 export const AdminPanel: React.FC<AdminPanelProps> = ({ id, debug }) => {
   const [state, setState] = useState<State>({ loading: false, data: null });
+
+  useEffect(() => {
+    if(debug) console.log('Component Mounted', id);
+  }, [id, debug]);
