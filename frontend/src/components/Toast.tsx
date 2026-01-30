4234f597bc86bda20a53bc0bea4a4ed2fcf68e7f
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 18:58:29 +0100

    feat: define types for Toast

--- frontend/src/components/Toast.tsx
+++ frontend/src/components/Toast.tsx
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styles from './Toast.module.css';
 
 // Toast Component Definition
+
+interface ToastProps {
+  title?: string;
+  isActive?: boolean;
+  onAction?: () => void;
+}
