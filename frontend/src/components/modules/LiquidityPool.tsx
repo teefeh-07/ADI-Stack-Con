903933e6145f7317afeadf53033764c14b04eec3
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:19:10 +0100

    feat: define generic props for LiquidityPool

--- frontend/src/components/modules/LiquidityPool.tsx
+++ frontend/src/components/modules/LiquidityPool.tsx
@@ -1,3 +1,8 @@
 // LiquidityPool Component Framework
 import React, { useEffect, useState } from 'react';
 import styles from './LiquidityPool.module.css';
+
+interface LiquidityPoolProps {
+  id: string;
+  debug?: boolean;
+}
