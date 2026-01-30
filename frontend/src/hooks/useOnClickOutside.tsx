1555bf0e93a0276dbfe7c08e542d946cd398687a
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:14:12 +0100

    feat: implement useOnClickOutside logic

--- frontend/src/hooks/useOnClickOutside.tsx
+++ frontend/src/hooks/useOnClickOutside.tsx
@@ -8,3 +8,14 @@ interface useOnClickOutsideProps {
   isActive?: boolean;
   onAction?: () => void;
 }
+
+export const useOnClickOutside: React.FC<useOnClickOutsideProps> = ({ title = 'useOnClickOutside', isActive, onAction }) => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className={styles.container}>
+      <h1>{title}</h1>
+      <button onClick={() => setCount(c => c + 1)}>Count: {count}</button>
+    </div>
+  );
+};
