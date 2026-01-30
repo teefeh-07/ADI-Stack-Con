990032c6510bb4b91f72569efd5497912df49e25
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 20:38:38 +0100

    i18n: add 'nav.about' to nl

--- frontend/src/locales/nl.ts
+++ frontend/src/locales/nl.ts
@@ -2,3 +2,4 @@
 export const messages = {
   "app.title": "ADI Stack Con - nl",
   "nav.home": "Home - nl",
+  "nav.about": "About - nl",
--- frontend/src/locales/ru.ts
+++ frontend/src/locales/ru.ts
@@ -1,3 +1,4 @@
 // Localization for Russian (ru)
 export const messages = {
   "app.title": "ADI Stack Con - ru",
+  "nav.home": "Home - ru",
