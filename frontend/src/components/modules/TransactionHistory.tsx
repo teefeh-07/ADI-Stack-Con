e5d4ce0953b1cd5721a0eb53048db2991774e975
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:23:28 +0100

    feat: define generic props for TransactionHistory

--- frontend/src/components/modules/TransactionHistory.tsx
+++ frontend/src/components/modules/TransactionHistory.tsx
@@ -1,3 +1,8 @@
 // TransactionHistory Component Framework
 import React, { useEffect, useState } from 'react';
 import styles from './TransactionHistory.module.css';
+
+interface TransactionHistoryProps {
+  id: string;
+  debug?: boolean;
+}
