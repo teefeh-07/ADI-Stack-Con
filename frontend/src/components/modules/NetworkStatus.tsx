e2203149b150b4f3070469b0ee170497adc57e2c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:28:02 +0100

    feat: add side-effect logic to NetworkStatus

--- frontend/src/components/modules/NetworkStatus.tsx
+++ frontend/src/components/modules/NetworkStatus.tsx
@@ -15,3 +15,7 @@ interface State {
 
 export const NetworkStatus: React.FC<NetworkStatusProps> = ({ id, debug }) => {
   const [state, setState] = useState<State>({ loading: false, data: null });
+
+  useEffect(() => {
+    if(debug) console.log('Component Mounted', id);
+  }, [id, debug]);
