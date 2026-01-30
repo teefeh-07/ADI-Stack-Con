af8c20fb8af98c3ed4ee4ad9beec20c3acf6a79d
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:16:00 +0100

    feat: define generic props for StakingDashboard

--- frontend/src/components/modules/StakingDashboard.tsx
+++ frontend/src/components/modules/StakingDashboard.tsx
@@ -1,3 +1,8 @@
 // StakingDashboard Component Framework
 import React, { useEffect, useState } from 'react';
 import styles from './StakingDashboard.module.css';
+
+interface StakingDashboardProps {
+  id: string;
+  debug?: boolean;
+}
