206c709f83615f476b6075ccd00a3465e9cfae41
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:31:03 +0100

    feat: add side-effect logic to AnalyticsChart

--- frontend/src/components/modules/AnalyticsChart.tsx
+++ frontend/src/components/modules/AnalyticsChart.tsx
@@ -15,3 +15,7 @@ interface State {
 
 export const AnalyticsChart: React.FC<AnalyticsChartProps> = ({ id, debug }) => {
   const [state, setState] = useState<State>({ loading: false, data: null });
+
+  useEffect(() => {
+    if(debug) console.log('Component Mounted', id);
+  }, [id, debug]);
