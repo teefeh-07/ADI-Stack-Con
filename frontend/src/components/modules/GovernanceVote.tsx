ffee7bc96ad7b0789c79c58fdd4ac3dc1f542b48
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:17:16 +0100

    feat: define generic props for GovernanceVote

--- frontend/src/components/modules/GovernanceVote.tsx
+++ frontend/src/components/modules/GovernanceVote.tsx
@@ -1,3 +1,8 @@
 // GovernanceVote Component Framework
 import React, { useEffect, useState } from 'react';
 import styles from './GovernanceVote.module.css';
+
+interface GovernanceVoteProps {
+  id: string;
+  debug?: boolean;
+}
