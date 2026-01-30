2227618a114f4dc15d625027791555d85287c6f0
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:11:54 +0100

    feat: define types for useLocalStorage

--- frontend/src/hooks/useLocalStorage.tsx
+++ frontend/src/hooks/useLocalStorage.tsx
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styles from './useLocalStorage.module.css';
 
 // useLocalStorage Component Definition
+
+interface useLocalStorageProps {
+  title?: string;
+  isActive?: boolean;
+  onAction?: () => void;
+}
