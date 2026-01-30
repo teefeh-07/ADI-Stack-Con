9df5b8b5d5d933b5d109e521b99e6e67bc55c526
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:25:17 +0100

    feat: define generic props for WalletSettings

--- frontend/src/components/modules/WalletSettings.tsx
+++ frontend/src/components/modules/WalletSettings.tsx
@@ -1,3 +1,8 @@
 // WalletSettings Component Framework
 import React, { useEffect, useState } from 'react';
 import styles from './WalletSettings.module.css';
+
+interface WalletSettingsProps {
+  id: string;
+  debug?: boolean;
+}
