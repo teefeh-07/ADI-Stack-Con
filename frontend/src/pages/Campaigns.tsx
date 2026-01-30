408fef6f9932aeffd096b4cb8023426ce916e220
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:06:09 +0100

    feat: define types for Campaigns

--- frontend/src/pages/Campaigns.tsx
+++ frontend/src/pages/Campaigns.tsx
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styles from './Campaigns.module.css';
 
 // Campaigns Component Definition
+
+interface CampaignsProps {
+  title?: string;
+  isActive?: boolean;
+  onAction?: () => void;
+}
