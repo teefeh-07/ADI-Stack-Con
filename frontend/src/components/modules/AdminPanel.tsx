f0cfd68b42a386a9238fd64d468b7153f3ed029e
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:30:00 +0100

    feat: define generic props for AdminPanel

--- frontend/src/components/modules/AdminPanel.tsx
+++ frontend/src/components/modules/AdminPanel.tsx
@@ -1,3 +1,8 @@
 // AdminPanel Component Framework
 import React, { useEffect, useState } from 'react';
 import styles from './AdminPanel.module.css';
+
+interface AdminPanelProps {
+  id: string;
+  debug?: boolean;
+}
