1d185207a6f87c86b4c5de6c4b13374067b8b46f
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:08:13 +0100

    feat: define types for FundDetails

--- frontend/src/pages/FundDetails.tsx
+++ frontend/src/pages/FundDetails.tsx
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styles from './FundDetails.module.css';
 
 // FundDetails Component Definition
+
+interface FundDetailsProps {
+  title?: string;
+  isActive?: boolean;
+  onAction?: () => void;
+}
