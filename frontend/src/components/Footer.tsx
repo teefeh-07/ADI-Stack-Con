6d1fdbbe3af71a8738b8b061c889522782e71637
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 18:42:10 +0100

    feat: define types for Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styles from './Footer.module.css';
 
 // Footer Component Definition
+
+interface FooterProps {
+  title?: string;
+  isActive?: boolean;
+  onAction?: () => void;
+}
