8fec0048cfdd7be408930c16374cb883a1849f74
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 20:32:26 +0100

    i18n: add 'btn.disconnect' to es

--- frontend/src/locales/es.ts
+++ frontend/src/locales/es.ts
@@ -3,3 +3,5 @@ export const messages = {
   "app.title": "ADI Stack Con - es",
   "nav.home": "Home - es",
   "nav.about": "About - es",
+  "btn.connect": "Connect Wallet - es",
+  "btn.disconnect": "Disconnect - es",
