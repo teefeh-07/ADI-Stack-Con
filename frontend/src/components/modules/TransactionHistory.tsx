56caa4807e074ac5eac640d0c809d9043f61ec7a
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-01-30 19:23:33 +0100

    feat: scaffold component function for TransactionHistory

--- frontend/src/components/modules/TransactionHistory.tsx
+++ frontend/src/components/modules/TransactionHistory.tsx
@@ -12,3 +12,6 @@ interface State {
   loading: boolean;
   data: any;
 }
+
+export const TransactionHistory: React.FC<TransactionHistoryProps> = ({ id, debug }) => {
+  const [state, setState] = useState<State>({ loading: false, data: null });
